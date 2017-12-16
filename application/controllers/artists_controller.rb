fd272c4ba7700bcc4c903dda7105773a5815b98a
Eric Carles <carles.eric@wanadoo.fr>
2017-12-16 22:53:38 +0000

    RestFul entries created for controllers : artists_controller, genres_controller and stock_albums_controller

---
+++ application/controllers/artists_controller.rb
@@ -0,0 +1,41 @@
+require( 'sinatra' )
+require( 'sinatra/contrib/all' )
+require( 'pry-byebug' )
+require_relative('./../models/artist')
+
+
+
+
+get '/mstore/artists' do
+
+
+end
+
+
+get '/mstore/artists/new' do
+
+
+end
+
+
+get '/mstore/artists/:art_name' do
+
+
+end
+
+
+
+get '/music/artists/:id/edit' do
+
+
+end
+
+post '/music/artists/:id' do
+
+
+end
+
+post '/music/artists/:id/delete' do
+
+
+end
---
+++ application/controllers/genres_controller.rb
@@ -0,0 +1,31 @@
+require( 'sinatra' )
+require( 'sinatra/contrib/all' )
+require( 'pry-byebug' )
+require_relative('./../models/genre')
+
+
+
+
+get '/mstore/genres' do
+
+end
+
+get '/mstore/genres/new' do
+
+end
+
+get '/mstore/genres/:genre' do
+
+end
+
+get '/music/genres/:id/edit' do
+
+end
+
+post '/music/genres/:id' do
+
+end
+
+post '/music/genres/:id/delete' do
+
+end
