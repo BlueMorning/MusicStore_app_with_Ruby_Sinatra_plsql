8965668068292cfc1c590d8c8cbfa95e00ddbf0f
Eric Carles <carles.eric@wanadoo.fr>
2017-12-16 19:14:16 +0000

    Class Helper, methos class run_sql_return_first_row_column_value added

--- application/helper/dbhelper.rb
+++ application/helper/dbhelper.rb
@@ -20,6 +20,22 @@ class DbHelper
     return result
   end
 
+  # sql: query to execute.
+  # params : array of the parameters used in the query.
+  # return the value of the given column of the first row
+  def self.run_sql_return_first_row_column_value(sql, params = [], column_name)
+    begin
+      db_connection = PG.connect({dbname: "music_store", host: "localhost"})
+      db_connection.prepare("query", sql)
+
+      result        = db_connection.exec_prepared("query", params)
+    ensure
+      db_connection.close() if db_connection != nil
+    end
+
+    return result[0][column_name]
+  end
+
   # array_of_hashes to turn into objects.
   # classname : name of the class that the hashes have to be turned into.
   # return an array of classname objects
