0bbd7f447630e60dc30af377a6360a6778eeccec
Eric Carles <carles.eric@wanadoo.fr>
2017-12-16 18:37:09 +0000

    Class Artist: insert(), update() and save() methods completed

--- application/models/artist.rb
+++ application/models/artist.rb
@@ -9,5 +9,30 @@ class Artist
   end
 
 
+  # Perform an insert or an update depending on the value of art_id
+  def save()
+    if(@art_id) #if the row already exists
+      update()
+    else
+      insert()
+    end
+  end
+
+
+
+  private
+
+  # Insert the artist in the Artist table
+  def insert()
+    query   = "INSERT INTO artists (art_name) VALUES ($1) RETURNING art_id"
+    @art_id = DbHelper.run_sql(query, [@art_name])
+  end
+
+  # Update the artist in the Artist table
+  def update()
+    query   = "UPDATE artists SET art_name = $1 WHERE art_id = $2"
+    @art_id = DbHelper.run_sql(query, [@art_name, @art_id])
+  end
+
 
 end
