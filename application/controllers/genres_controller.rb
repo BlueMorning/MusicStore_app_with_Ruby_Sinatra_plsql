3b4f21ee8005cc6fc172edc9cf0db3f94da29115
Eric Carles <carles.eric@wanadoo.fr>
2017-12-17 20:35:03 +0000

    genres_controller completed

--- application/controllers/genres_controller.rb
+++ application/controllers/genres_controller.rb
@@ -2,30 +2,58 @@ require( 'sinatra' )
 require( 'sinatra/contrib/all' )
 require( 'pry-byebug' )
 require_relative('./../models/genre')
+require_relative('./../helper/navigation')
 
 
-
-
-get '/mstore/genres' do
-
+# Display the list of genres
+get NavGenres::GET_INDEX do
+  @gen_name = params.include?('gen_name') ? params['gen_name'] : ""
+  @genres   = Genre.find_all()
+  erb(:"genres/index")
 end
 
-get '/mstore/genres/new' do
-
+# Form to create a new genre
+get NavGenres::GET_NEW do
+  @genre              = Genre.new({"gen_name" => ""})
+  @form_title         = "New Genre"
+  @form_action        = NavGenres::POST_NEW
+  @form_submit_label  = "Save"
+  erb(:"genres/new_or_edit")
 end
 
-get '/mstore/genres/:genre' do
-
+# Create a new genre from the form
+post NavGenres::GET_INDEX do
+  @genre = Genre.new(params)
+  @genre.save()
+  redirect(:"#{NavGenres::GET_INDEX}")
 end
 
-get '/music/genres/:id/edit' do
-
+# Display the genres whose name match with the research
+get NavGenres::GET_WITH_FILTERS do
+  @gen_name = params['gen_name']
+  @genres  = Genre.search_all_by_name(@gen_name)
+  erb(:"genres/index")
 end
 
-post '/music/genres/:id' do
 
+# Form to edit an genre
+get NavGenres::GET_EDIT_BY_ID do
+  @genre              = Genre.find_by_id(params['gen_id'])
+  @form_title         = "Modify Genre"
+  @form_action        = NavGenres.nav_post_update_by_id(@genre.gen_id)
+  @form_submit_label  = "Update"
+  erb(:"genres/new_or_edit")
 end
 
-post '/music/genres/:id/delete' do
+# Update the modifications on the genre
+post NavGenres::POST_UPDATE_BY_ID do
+  @genre = Genre.new(params)
+  @genre.save()
+  redirect(:"#{NavGenres::GET_INDEX}")
+end
 
+# Delete the genre on its id
+post NavGenres::POST_DELETE_BY_ID do
+  Genre.delete_by_id(params['gen_id'])
+  redirect(:"#{NavGenres::GET_INDEX}")
 end
