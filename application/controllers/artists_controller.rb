2da6e83999972cb313c32cf1c2fdea119536c983
Eric Carles <carles.eric@wanadoo.fr>
2017-12-17 11:44:30 +0000

    artists_controller refactored with navigation routes constants

--- application/controllers/artists_controller.rb
+++ application/controllers/artists_controller.rb
@@ -2,40 +2,42 @@ require( 'sinatra' )
 require( 'sinatra/contrib/all' )
 require( 'pry-byebug' )
 require_relative('./../models/artist')
+require_relative('./../helper/navigation')
 
 
+# Display the stock of albums
+get NavArtists::GET_ARTISTS_INDEX do
+
+  p "YOU ARE HERE #{NavArtists::GET_ARTISTS_INDEX}"
+end
+
+# Form to create a new album entry in the stock
+get NavArtists::GET_ARTISTS_NEW do
 
 
-get '/mstore/artists' do
+end
+
+# Display the stocks of albums whose name match with the research
+get NavArtists::GET_ARTISTS_BY_NAME do
 
 
 end
 
 
-get '/mstore/artists/new' do
+# Form to edit an album
+get NavArtists::GET_ARTISTS_EDIT_BY_ID do
 
 
 end
 
-
-get '/mstore/artists/:art_name' do
-
-
-end
-
-
-
-get '/music/artists/:id/edit' do
+# Update the modifications of the album
+post NavArtists::POST_ARTISTS_UPDATE_BY_ID do
 
 
 end
 
-post '/music/artists/:id' do
-
-
-end
-
-post '/music/artists/:id/delete' do
+# Delete the album on its id
+post NavArtists::POST_ARTISTS_DELETE_BY_ID do
 
 
 end
