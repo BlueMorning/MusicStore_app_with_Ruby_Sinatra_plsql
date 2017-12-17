d92bd67650cb104a6761e2cee533673d29a15468
Eric Carles <carles.eric@wanadoo.fr>
2017-12-17 15:51:50 +0000

    Artists controller entries completed for index and serach by name

--- application/controllers/artists_controller.rb
+++ application/controllers/artists_controller.rb
@@ -6,38 +6,40 @@ require_relative('./../helper/navigation')
 
 
 # Display the stock of albums
-get NavArtists::GET_ARTISTS_INDEX do
-
-  p "YOU ARE HERE #{NavArtists::GET_ARTISTS_INDEX}"
+get NavArtists::GET_INDEX do
+  @art_name = params.include?('art_name') ? params['art_name'] : ""
+  @artists  = Artist.find_all()
+  erb(:"artists/index")
 end
 
 # Form to create a new album entry in the stock
-get NavArtists::GET_ARTISTS_NEW do
+get NavArtists::GET_NEW do
 
 
 end
 
 # Display the stocks of albums whose name match with the research
-get NavArtists::GET_ARTISTS_BY_NAME do
-
-
+get NavArtists::GET_BY_NAME do
+  @art_name = params['art_name']
+  @artists  = Artist.search_all_by_name(@art_name)
+  erb(:"artists/index")
 end
 
 
 # Form to edit an album
-get NavArtists::GET_ARTISTS_EDIT_BY_ID do
+get NavArtists::GET_EDIT_BY_ID do
 
 
 end
 
 # Update the modifications of the album
-post NavArtists::POST_ARTISTS_UPDATE_BY_ID do
+post NavArtists::POST_UPDATE_BY_ID do
 
 
 end
 
 # Delete the album on its id
-post NavArtists::POST_ARTISTS_DELETE_BY_ID do
+post NavArtists::POST_DELETE_BY_ID do
 
 
 end
