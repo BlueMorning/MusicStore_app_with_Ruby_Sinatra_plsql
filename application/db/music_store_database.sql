d02a7ec3d7044457666549afe69a08fa8d4ee9e2
Eric Carles <carles.eric@wanadoo.fr>
2017-12-19 20:13:06 +0000

    New tables sale_orders and purchase_orders

--- application/db/music_store_database.sql
+++ application/db/music_store_database.sql
@@ -1,5 +1,7 @@
-DROP TABLE IF EXISTS purchases;
-DROP TABLE IF EXISTS sales;
+DROP TABLE IF EXISTS purchases_items;
+DROP TABLE IF EXISTS purchases_orders;
+DROP TABLE IF EXISTS sale_items;
+DROP TABLE IF EXISTS sale_orders;
 DROP TABLE IF EXISTS suppliers;
 DROP TABLE IF EXISTS customers;
 DROP TABLE IF EXISTS albums;
@@ -44,24 +46,41 @@ CREATE TABLE customers (
   cus_name VARCHAR(50) UNIQUE NOT NULL
 );
 
--- Table purchases
-CREATE TABLE purchases (
-  pur_id SERIAL4 PRIMARY KEY,
-  pur_qty INT2 NOT NULL,
-  pur_unit_price INT2 NOT NULL,
-  pur_total_price INT2 NOT NULL,
-  pur_date DATE NOT NULL,
-  pur_sup_id INT4 REFERENCES suppliers(sup_id),
-  pur_alb_id INT4 REFERENCES albums(alb_id)
+
+-- Table purchases orders
+CREATE TABLE purchase_orders (
+  pro_id SERIAL4 PRIMARY KEY,
+  pro_sup_id INT4 REFERENCES suppliers(sup_id),
+  pro_total_price INT2 NOT NULL,
+  pro_date DATE NOT NULL,
+  pro_statuts VARCHAR(1)
 );
 
--- Table sales
-CREATE TABLE sales (
-  sal_id SERIAL4 PRIMARY KEY,
-  sal_qty INT2 NOT NULL,
-  sal_unit_price INT2 NOT NULL,
-  sal_total_price INT2 NOT NULL,
-  sal_date DATE NOT NULL,
-  sal_cus_id INT4 REFERENCES customers(cus_id),
-  sal_alb_id INT4 REFERENCES albums(alb_id)
+-- Table purchases items
+CREATE TABLE purchase_items (
+  pri_id SERIAL4 PRIMARY KEY,
+  pri_qty INT2 NOT NULL,
+  pri_unit_price INT2 NOT NULL,
+  pri_alb_id INT4 REFERENCES albums(alb_id),
+  pri_pro_id INT4 REFERENCES purchase_orders(pro_id)
+);
+
+
+
+-- Table sales_main
+CREATE TABLE sale_orders (
+  slo_id SERIAL4 PRIMARY KEY,
+  slo_cus_id INT4 REFERENCES customers(cus_id),
+  slo_total_price INT2 NOT NULL,
+  slo_date DATE NOT NULL,
+  slo_status VARCHAR(1)
+);
+
+-- Table sales_items
+CREATE TABLE sale_items (
+  sli_id SERIAL4 PRIMARY KEY,
+  sli_qty INT2 NOT NULL,
+  sli_unit_price INT2 NOT NULL,
+  sli_alb_id INT4 REFERENCES albums(alb_id),
+  sli_slo_id INT4 REFERENCES sale_orders(slo_id)
 );
